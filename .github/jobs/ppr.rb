ab2c461680fd59d890590a5e6a02169ca187983a
denilsonpalhares <denilsonpalhares@gmail.com>
2023-02-15 18:01:08 -0300

    teste run

--- .github/jobs/ppr.rb
+++ .github/jobs/ppr.rb
@@ -1,13 +1,13 @@
 def run_rubocop_all
-  response = `rubocop `
+  `rubocop `
 end
 
 def run_rubocop_fast
-  response = `rubocop --fail-fast    `
+  `rubocop --fail-fast    `
 end
 
 
 param = ARGV[0]
 puts param
 
-run_rubocop_fast
+puts run_rubocop_fast
