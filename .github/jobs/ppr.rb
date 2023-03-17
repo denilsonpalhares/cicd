c7867c6794f75a8d1b16ec94480c073b69150389
denilsonpalhares <denilsonpalhares@gmail.com>
2023-03-17 18:04:19 -0300

    t2

--- .github/jobs/ppr.rb
+++ .github/jobs/ppr.rb
@@ -10,9 +10,11 @@ end
 param = ARGV[0]
 puts param
 
-puts ENV["RAIS_MASTER_KEY"]
-puts ENV["TESTE_MEU_TELEFONE"]
 
+puts "maste_key"
+puts ENV["RAIS_MASTER_KEY"]
+puts "tel"
+puts ENV["TESTE_MEU_TELEFONE"]
 exit 2 unless run_rubocop_fast
 
 p "OK"
