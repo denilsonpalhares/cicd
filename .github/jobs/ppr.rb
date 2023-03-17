e08b50b26345875ae586f864145e1548e07f38a8
denilsonpalhares <denilsonpalhares@gmail.com>
2023-03-17 18:27:33 -0300

    t6

--- .github/jobs/ppr.rb
+++ .github/jobs/ppr.rb
@@ -12,7 +12,10 @@ puts param
 
 
 puts "maste_key #{ENV["RAILS_MASTER_KEY"]}."
+puts ENV["RAILS_MASTER_KEY"] == "267befda0092784dc47464b146a9c7c8"
+puts
 puts "segredo #{ENV["SEGREDO"]}."
+puts ENV["SEGREDO"] == "e segredo"
 
 puts "tel #{ENV["TESTE_MEU_TELEFONE"]}." 
 exit 2 unless run_rubocop_fast
