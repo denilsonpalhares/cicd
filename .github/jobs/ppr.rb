3735c55e9336af3e45eae02eb7f66ec32b11d6b0
denilsonpalhares <denilsonpalhares@gmail.com>
2023-03-20 09:13:39 -0300

    t12

--- .github/jobs/ppr.rb
+++ .github/jobs/ppr.rb
@@ -12,7 +12,8 @@ puts param
 
 puts "RAILS_MASTER_KEY #{ENV["RAILS_MASTER_KEY"]}."
 puts ENV["RAILS_MASTER_KEY"] == "267befda0092784dc47464b146a9c7c8"
-puts "env",Cicd::Application.credentials.config
+puts "env"
+puts Cicd::Application.credentials.config
 
 puts "tel #{ENV["TESTE_MEU_TELEFONE"]}." 
 exit 2 unless run_rubocop_fast
