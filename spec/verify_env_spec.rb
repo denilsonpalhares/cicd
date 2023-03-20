90628bbf870d56b44b4bd2a353611d8a6d68b230
denilsonpalhares <denilsonpalhares@gmail.com>
2023-03-20 16:01:52 -0300

    t16

--- spec/verify_env_spec.rb
+++ spec/verify_env_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Rails do
   context "verify var env" do
     it "Meu telefone" do
       var = ENV["teste_meu_telefone"]
-      expect(var).to eq("12345")
+      expect(var).to eq("123457")
     end
   end
   
