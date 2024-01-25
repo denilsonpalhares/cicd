49140642bb04a0aa50067af0c28160de3577603b
denilson palhares <denilsonpalhares@gmail.com>
2024-01-25 18:08:16 -0300

    rwe

--- spec/verify_env_spec.rb
+++ spec/verify_env_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe Rails do
   context "verify var env" do
     it "Meu telefone" do
       var = ENV["teste_meu_telefone"]
+      10.times do |i|
+        puts i
+      end
       expect(var).to eq("123457")
     end
   end
